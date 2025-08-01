7fe936a578b3dcfe8438997b90602dae80befcb4
Yugg09 <yugchoudhary947@gmail.com>
2025-08-01 19:37:03 +0000

    datatypes

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,26 @@
+"use stirct";  //treat all js code as newer version
+
+ //alert(3+3) we are using node js not browser
+  console.log(3+3)
+  
+  
+
+  let name = "yug"
+  let age  = 18
+  let isLoggedIn = false
+
+  //primitive data types
+  //number => 2 to power 53
+  //bigint jb no bda ho
+  //string => ""
+  //boolean => true/false
+  //null => standalone value matlab empty (ek object h)
+  //undefined => 
+  //symbol => uniqueness find karne k liye
+
+  //object
+
+  console.log(typeof "yug");
+  console.log(typeof null); //object
+  console.log(typeof undefined); //undefined
+  
