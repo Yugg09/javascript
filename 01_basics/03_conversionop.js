90b411152b45c9ddc4993ca64a679f815a908433
Yugg09 <yugchoudhary947@gmail.com>
2025-08-02 09:25:02 +0000

    03_conversionop

--- 01_basics/03_conversionop.js
+++ 01_basics/03_conversionop.js
@@ -23,5 +23,36 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+//***************  OPERATIONS*********
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2**2); //2 ki pow 2
+
+let str1 = "hello"
+let str2 = " yug"
+let str3 = str1+str2
+//console.log(str3);
+
+//PROBLEN
+//console.log("1" + 2 + 2); //122
+//console.log(1 + 2 + "2"); //32
+
+//console.log(+true); //1
+
+//PREFIX AND POSTFIX
+let gameCounter = 100
+gameCounter++;
+//console.log(gameCounter);
+
+
+
+
+
+
+
