62891ba314adaa5e3cc95da8f5a9c7cd08033b27
Yugg09 <yugchoudhary947@gmail.com>
2025-08-02 08:39:32 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionop.js
@@ -0,0 +1,27 @@
+let score = "33"
+
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+
+//while converting
+//"33" > 33
+//"33ab" > NaN
+//TRUE > 1
+ 
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1 > true ; 0 > false
+//"" > false 
+//"yug" > true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
