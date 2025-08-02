26000141c43de80015dd410b46e5b424ae7442dc
Yugg09 <yugchoudhary947@gmail.com>
2025-08-02 09:37:47 +0000

    comparison operators

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,19 @@
+console.log(null > 0); //false
+console.log(null == 0); //false
+console.log(null >= 0); //true
+
+//== & <> >= <= work differrently
+//comparison converts null to a number treating it as 0
+// thats why >= 0 is true
+// == means quality check
+
+console.log(undefined ==0); //sabme false ayega
+
+//STRICT CHECK === (datatype bhi check krt h)
+
+console.log("2" === 2);
+
+
+
+
+
