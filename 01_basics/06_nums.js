4797d073782630049e6338fa54b18adaf9b9dbfe
Yug <yugchoudhary947@gmail.com>
2025-08-06 19:09:40 +0000

    maths info

--- 01_basics/06_nums.js
+++ 01_basics/06_nums.js
@@ -1,7 +1,47 @@
 const balance = new Number(400)
+<<<<<<< HEAD
 console.log(balance);
 
 console.log(balance.toString().length);
 console.log(balance.toFixed(2)); //for precision
+=======
+//console.log(balance);
+
+console.log(balance.toString().length);
+//console.log(balance.toFixed(2)); //for precision
+ 
+const otherNumber = 123.8966
+//console.log(otherNumber.toPrecision(4));
+
+const hundreds = 10000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+//***************** maths ***************//
+//maths is a object itself
+
+//console.log(Math.abs(-4)); //-ve to +ve
+//console.log(Math.round(4.6));
+//console.log(Math.ceil(4.2));// 4 se thodi bhi upar vali value choose hogi
+//console.log(Math.floor(4.9));
+//console.log(Math.sqrt(25));
+//console.log(Math.min(4,5,7,3));
+//max
+
+console.log((Math.random()*10)+1);
+
+const min  = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
+
+
+
+
+
+
+>>>>>>> 17efaa9 (maths info)
 
 
