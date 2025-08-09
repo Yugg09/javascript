aadad4d6a9ee3e9beac261fac6fe1ba852b0f3cf
Yug <yugchoudhary947@gmail.com>
2025-08-09 10:29:22 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,34 @@
+//array
+const myArr = [0,1,2,3,4,5,true,"yug"] //js resisiable arr
+console.log(myArr[0]);
+
+//array define
+const arr2 = new Array(1,2,3,4)
+
+//array methods
+//myArr.push(6)
+console.log(myArr);
+//myArr.pop()//removes last element
+
+//myArr.unshift(9)
+//myArr.shift()
+//console.log(myArr);
+
+//console.log(myArr.includes(9));
+
+const newArr = myArr.join() //change in type string
+console.log(newArr);
+
+//SLICE OR SPLICE
+
+console.log("A", myArr);
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B", myArr);
+
+
+
+
+
+
