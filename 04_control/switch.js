471add728c1522ff40f08d6030844e35d74cf17e
Yug <yugchoudhary947@gmail.com>
2025-08-24 11:35:02 +0000

    switch case statement

---
+++ 04_control/switch.js
@@ -0,0 +1,27 @@
+//switch (key) {
+  //  case value:
+        
+    //    break;
+
+    //default:
+      //  break;
+//}
+
+//SWITCH
+const month = "mar"
+ switch (month) {
+    case "mar":                        // shift +opt+ down arrow
+        console.log("january");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "jan":
+        console.log("march");
+        break;
+ 
+    default:
+        console.log("default case match");
+        
+        break;
+ }
