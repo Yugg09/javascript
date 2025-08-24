1d93b35c18f981751dabe4c5ce67deeb39d52d6f
Yug <yugchoudhary947@gmail.com>
2025-08-24 17:07:26 +0000

    for in loops

---
+++ 05_iterations/four.js
@@ -0,0 +1,12 @@
+// for in is for objects
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+for (const key in myObject) {
+    console.log(key,myObject[key]); //for off me seedha values aati thi
+    // for in keys deta h
+   
+}
