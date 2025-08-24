882443dbd47fedcfa859f38758767300ebc66ea0
Yug <yugchoudhary947@gmail.com>
2025-08-24 11:30:24 +0000

    if & else statement

---
+++ 04_control/one.js
@@ -0,0 +1,18 @@
+//poora code conditional base p run hona chaiye
+//if
+const isUserloggedIn = true
+
+const temperature = 40
+if (temperature < 50) {
+    console.log("executed");
+}
+else{
+console.log("grater than 50");
+}
+//>, >, <==
+// === type check
+
+const balance = 1000
+//if(balance > 500) console.log("test"); //implicit
+
+
